14a9e81e6e2ed524c5dfd93e198317159a18dfce
laboratorio <laboratorio@UPOAULA10607>
2024-05-22 08:01:24 -0500

    dsgfg

---
+++ TrabajoEnClase/Models/Note.cs
@@ -0,0 +1,9 @@
+namespace TrabajoEnClase.Models;
+
+
+internal class Note
+{
+    public string Filename { get; set; }
+    public string Text { get; set; }
+    public DateTime Date { get; set; }
+}
